58bfd9dc4d03012ed9e59dc21e6b2e098f9476f8
WenHan Gu <Wenhan.gu@mediatek.com>
2014-07-30 15:17:49 +0800

    Implement __aeabi_idiv0 and __aeabi_uidiv0.
    Current arm implementation needs these functions defined inside libgcc.a.
    We implement it simply by raising a SIGFPE signal.
    
    Change-Id: I748f7a0c9c7518082e1b8e091b0d53c8084289bf

---
+++ lib/builtins/arm/idiv0.c
@@ -0,0 +1,28 @@
+/*===-- idiv0.c - 32-bit unsigned integer divide --------------------------===//
+ *
+ *                     The LLVM Compiler Infrastructure
+ *
+ * This file is dual licensed under the MIT and the University of Illinois Open
+ * Source Licenses. See LICENSE.TXT for details.
+ *
+ *===----------------------------------------------------------------------===//
+ *
+ * This file implements the __aeabi_idiv0 and __aeabi_uidiv0
+ * function for the ARM 32-bit architecture.
+ *
+ *===----------------------------------------------------------------------===*/
+
+
+#include <signal.h>
+
+#ifdef __ARM_EABI__
+
+int __aeabi_idiv0(void) {
+  return raise(SIGFPE);
+}
+
+int __aeabi_uidiv0(void) {
+  return raise(SIGFPE);
+}
+
+#endif
